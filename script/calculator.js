0d0814e93639c701b83f94101db37a35d60fa22a
Thiago Henrique <thiagohenriquedev@gmail.com>
2021-11-13 15:19:39 -0200

    up

--- script/calculator.js
+++ script/calculator.js
@@ -1,3 +1,5 @@
-let calculadroa = new CalcController()
+window.calculator = new CalcController()
+
+
 
 
--- script/controller/calcController.js
+++ script/controller/calcController.js
@@ -1,28 +1,48 @@
 class CalcController {
-    constructor( ){
-         this._displayCalc = "0";
+    constructor(){
+
+         this._displayCalcEl = document.querySelector("#display");
+         this._dateEl = document.querySelector("#data");
+         this._timeEl = document.querySelector("#hora");
          this._currentDate;
          this.inniitialize();
 
     }
 
     inniitialize(){
+        
+        
 
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+
+    }
+    get displayDate(){
+        return this._dateEl.innerHTML
+    }
+    
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
     }
 
     get displayCalc(){
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
     
-    set displayCalc(valor){
-        this._displayCalc = valor;
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
     }
 
     get currentDate(){
-        return this._dataAtual
+        return new Date();
     }
 
-    set currentData(valor){
-        this.currentDate = valor;
+    set currentDate(value){
+        this.currentDate = value;
     }
 }
